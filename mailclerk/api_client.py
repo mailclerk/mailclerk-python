04b25f9cd9923256bcae7d9cacdb44a30bd3b729
Gregory Sherrid <>
2021-05-14 22:17:37 -0400

    python skd prep

--- mailclerk/api_client.py
+++ mailclerk/api_client.py
@@ -1,39 +1,35 @@
 import requests, base64
 
-from mailclerk import __version__
-
-class MailclerkError(Exception):
-    pass
-    
-class MailclerkAPIError(MailclerkError):
-    def __init__(self, description, http_status=None, http_response=None):
-        super(MailclerkError, self).__init__(description)
-        
-        self.http_status = http_status
-        self.http_response = http_response
-    
+from mailclerk import __version__, outbox
+from .errors import MailclerkError, MailclerkAPIError
 
 class MailclerkAPIClient():
     def __init__(self, api_key, api_url):
         self.api_url = api_url
         self.api_key = api_key
         
-        self.version_label = "Mailclerk Python %s" % __version__
-        
-    def deliver(self, template, recipient, data = {}, options = {}):
         if self.api_key == None or self.api_key == "":
             raise MailclerkError("No Mailclerk API Key provided. Set `mailclerk.api_key`")
-            
+
+        self.version_label = "Mailclerk Python %s" % __version__
+        
+    def deliver(self, template, recipient, data = {}, options = {}):            
         token = base64.b64encode(("%s:" % self.api_key).encode("utf-8")).decode('utf-8')
         
+        if outbox.enabled:
+            options = dict(options)
+            options["local_outbox"] = True
+        
+        params = {
+            "template": template,
+            "recipient": recipient,
+            "data": data,
+            "options": options
+        }
+
         response = requests.post(
             "%s/deliver" % self.api_url,
-            json={
-                "template": template,
-                "recipient": recipient,
-                "data": data,
-                "options": options
-            },
+            json=params,
             headers={
                 'X-Client-Version': self.version_label,
                 'Authorization': "Basic %s" % token
@@ -51,5 +47,8 @@ class MailclerkAPIClient():
                 http_status=response.status_code,
                 http_response=response
             )
+            
+        if outbox.enabled:
+            outbox.add_send(params, response.json()["delivery"])
         
         return response
